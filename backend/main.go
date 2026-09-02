0374ab77c7421d1747c7c675c0d3403a55e707a7
Hugo ARNAL <hugo@hugoarnal.com>
2026-09-02 09:31:09 +0200

    feat: admin middleware

--- backend/main.go
+++ backend/main.go
@@ -33,8 +33,8 @@ func main() {
 
 	survey := router.Group("/survey")
 	survey.GET("", middlewares.AuthMiddleware, routes.SurveyGetHandler)
-	survey.POST("", middlewares.AuthMiddleware, routes.SurveyPostHandler)
-	survey.PUT("", middlewares.AuthMiddleware, routes.SurveyPutHandler)
+	survey.POST("", middlewares.AuthMiddleware, middlewares.AdminMiddleware, routes.SurveyPostHandler)
+	survey.PUT("", middlewares.AuthMiddleware, middlewares.AdminMiddleware, routes.SurveyPutHandler)
 
 	router.GET("/ping", routes.PingHandler)
 	router.GET("/users/:id", routes.GetUser(database.DB))
@@ -44,6 +44,7 @@ func main() {
 	router.GET("/survey", routes.GetSurvey(database.DB))
 
 	router.GET("/ping/auth", middlewares.AuthMiddleware, routes.PingHandler)
+	router.GET("/ping/admin", middlewares.AuthMiddleware, middlewares.AdminMiddleware, routes.PingHandler)
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 
 	err := router.Run(":8080")
