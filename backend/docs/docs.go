59167e760d9651eee1df6ee48acfa711acf03177
Hugo ARNAL <hugo@hugoarnal.com>
2026-09-08 17:42:17 +0200

    ref: only get job seekers from paginated users endpoint

--- backend/docs/docs.go
+++ backend/docs/docs.go
@@ -395,7 +395,7 @@ const docTemplate = `{
         },
         "/users": {
             "get": {
-                "description": "Get all users (paginated). Users are filtered by their latest profile update then by their id.",
+                "description": "Get all users (paginated). Users are filtered by their latest profile update then by their id. Only job seekers are returned from this endpoint.",
                 "consumes": [
                     "application/json"
                 ],
